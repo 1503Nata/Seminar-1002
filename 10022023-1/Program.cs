a060de485350425f181fcd337c1ba41dbdd22cc2
Natalya <pauls88@yandex.ru>
2023-02-15 18:13:47 +0500

    AddFile10022023-1

---
+++ 10022023-1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.");
+Console.WriteLine();
+Console.WriteLine("Введите значение числа N для построения ряда натуральных чисел:  ");
+int n= Convert.ToInt32(Console.ReadLine());
+{if  (n<=0) 
+            {   Console.Write("Вы ввели не натуральное число.Повторите попытку.");
+                return;
+             }
+}
+{
+void NaturNumber (int n)
+{if (n>0)  
+            {   
+                Console.Write(n + ",");
+                 if (n==1) {Console.Write(".");}
+                NaturNumber (n-1);
+                return;
+            }
+}        
+Console.Write($"Ряд натуральных чисел от {n} до 1: ");
+
+NaturNumber(n);
+}
